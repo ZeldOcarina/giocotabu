77aaa6a182e83f7709a4e24fbd168eb012f46fbe
unknown <mattia.r86@gmail.com>
2020-07-15 17:28:14 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import "./styles/index.scss";
+
+/*window.addEventListener("load", () => {
+  var b = document.getElementsByClassName("addToCartBtn");
+  for (var i = 0; i < b.length; i++)
+    b[i].onclick = (e) =>
+      addToCart(e.currentTarget.dataset.productId, function () {
+        window.location.href = "https://glxnow.com/checkout/1068";
+      });
+});
+
+submitFormToSalesJet();
+createCards();*/
