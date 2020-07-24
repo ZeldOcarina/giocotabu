f43334f1a7c1bfbe1a202d9fac031e6479008f7f
unknown <mattia.r86@gmail.com>
2020-07-24 11:27:30 +0200

    Complete website

--- src/index.js
+++ src/index.js
@@ -4,16 +4,4 @@ import "./js/responsive-nav";
 import submitFormToSalesJet from "./js/submitToSalesJet";
 
 submitFormToSalesJet();
-
 document.getElementById("date").innerText = new Date().getFullYear();
-/*window.addEventListener("load", () => {
-  var b = document.getElementsByClassName("addToCartBtn");
-  for (var i = 0; i < b.length; i++)
-    b[i].onclick = (e) =>
-      addToCart(e.currentTarget.dataset.productId, function () {
-        window.location.href = "https://glxnow.com/checkout/1068";
-      });
-});
-
-submitFormToSalesJet();
-createCards();*/
--- src/js/submitToSalesJet.js
+++ src/js/submitToSalesJet.js
@@ -1,36 +1,55 @@
+import formurlencoded from "form-urlencoded";
+
 export default function () {
   const popup = document.querySelector(".popup");
   const orderSpan = document.querySelector(".order_name");
   const popupContainer = document.querySelector(".popup__container");
+  const form = document.querySelector("form");
+  const cardBtn = document.querySelector("#card");
+  const loader = document.querySelector(".loader");
+
+  let btnClicked = false;
+
   popup.addEventListener("click", () => {
     popup.classList.add("d-none");
   });
 
-  const form = document.querySelector("form");
+  cardBtn.addEventListener("click", () => {
+    btnClicked = true;
+  });
+
   if (form) {
     form.addEventListener("submit", async function (e) {
       e.preventDefault();
+      loader.classList.remove("d-none");
       const formData = new FormData(form);
 
       const requestBody = {};
       for (var [name, value] of formData.entries()) requestBody[name] = value;
+      requestBody.accept = true;
 
       try {
         const response = await fetch("/f/1155-4702", {
           method: "POST",
           headers: {
-            "Content-Type": "application/json",
+            "Content-Type": "application/x-www-form-urlencoded",
           },
-          body: JSON.stringify(requestBody),
+          body: formurlencoded(requestBody),
         });
-
         if (response.status === 200) {
-          popup.classList.remove("d-none");
-          orderSpan.innerText = `${requestBody.first_name} ${requestBody.last_name}`;
+          loader.classList.add("d-none");
+          if (btnClicked) {
+            addToCart(cardBtn.dataset.productId, function () {
+              window.location.pathname = "/checkout/1155";
+            });
+          } else {
+            popup.classList.remove("d-none");
+            orderSpan.innerText = `${requestBody.first_name} ${requestBody.last_name}`;
+          }
         } else throw new Error("Messaggio non Inviato");
       } catch (err) {
         console.dir(err);
-        console.log(popupContainer);
+        loader.classList.add("d-none");
         popupContainer.innerHTML = /*html*/ `
           <h1 class="popup__title red">Il messaggio non è stato registrato</h1>
           <p class="popup__paragraph">Per favore scrivi a giocotabu@gmail.com per effettuare il tuo ordine.</p>
@@ -39,10 +58,4 @@ export default function () {
       }
     });
   }
-
-  // Checkout Page:
-  // /checkout/1155
-
-  // Product-Id:
-  //
 }
