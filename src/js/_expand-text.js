b0c805b66fc60d8bcff81b1cf98c41b0e1f6a1a6
unknown <mattia.r86@gmail.com>
2020-10-14 21:19:00 +0200

    Complete tab-land media queries

--- src/js/_expand-text.js
+++ src/js/_expand-text.js
@@ -1,16 +1,19 @@
-const btn = document.querySelector(".header__expand-icon");
-const div = document.querySelector(".buy-points");
-const svg = document.querySelector(".header__expand-icon--svg");
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.querySelector(".header__expand-icon");
+  const div = document.querySelector(".buy-points");
+  const svg = document.querySelector(".header__expand-icon--svg");
 
-let opened = false;
+  let opened = false;
 
-btn.addEventListener("click", () => {
-  opened = !opened;
-  if (opened) {
+  btn.addEventListener("click", () => {
+    opened = !opened;
+    if (opened) {
+      svg.src =
+        "https://cdn.salesjet.io/850/funnel_files/1155/271_right-arrow.svg";
+      return div.classList.remove("d-none");
+    }
     svg.src =
-      "https://cdn.salesjet.io/850/funnel_files/1155/271_right-arrow.svg";
-    return div.classList.remove("d-none");
-  }
-  svg.src = "https://cdn.salesjet.io/850/funnel_files/1155/270_down-arrow.svg";
-  return div.classList.add("d-none");
+      "https://cdn.salesjet.io/850/funnel_files/1155/270_down-arrow.svg";
+    return div.classList.add("d-none");
+  });
 });
